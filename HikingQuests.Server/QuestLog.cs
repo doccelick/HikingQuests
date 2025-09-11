891ab41f894672959654102a2d07031d5afa1336
Anders Evjen Bakke <aeb@ananki.no>
2025-09-11 21:22:13 +0200

    Added exception handling for QuestLog

--- HikingQuests.Server/QuestLog.cs
+++ HikingQuests.Server/QuestLog.cs
@@ -7,14 +7,37 @@
         private Dictionary<Guid, QuestItem> _questItems = new Dictionary<Guid, QuestItem>();
         public IEnumerable<QuestItem> QuestItems => _questItems.Values;
 
-        public void AddQuest(QuestItem questItem)
-        {            
+        public void AddQuest(QuestItem? questItem)
+        {
+            if (questItem == null)
+            {
+                throw new ArgumentNullException(nameof(questItem));
+            }
+
+            if (_questItems.ContainsKey(questItem.Id))
+            {
+                throw new InvalidOperationException(QuestMessages.QuestAlreadyExistsInLog);
+            }
             _questItems[questItem.Id] = questItem;
         }
 
         public QuestItem GetQuestById(Guid questId)
-        {            
+        {
+            if (!_questItems.ContainsKey(questId))
+            {
+                throw new KeyNotFoundException(QuestMessages.QuestNotFound);
+            }
             return _questItems[questId];
+        }
+
+        public QuestItem GetQuestByTitle(string title)
+        {
+            var questItem = _questItems.Values.FirstOrDefault(q => q.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (questItem == null)
+            {
+                throw new KeyNotFoundException(QuestMessages.QuestNotFound);
+            }
+            return questItem;
         }
     }
 }
