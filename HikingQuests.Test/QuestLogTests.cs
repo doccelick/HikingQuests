013d5eb295aa8b1d90a2199d3bbdc7940deabc39
Anders Evjen Bakke <aeb@ananki.no>
2025-09-11 18:42:18 +0200

    Added initial tests for creation of QuestItem in QuestLog

---
+++ HikingQuests.Test/QuestLogTests.cs
@@ -0,0 +1,42 @@
+﻿using HikingQuests.Server;
+
+namespace HikingQuests.Test
+{
+    public class QuestLogTests
+    {
+        [Fact]
+        public void QuestLog_Initial_Quest_Count_Is_Zero()
+        {
+            var questLog = new QuestLog();
+            var questCount = questLog.QuestItems.Count();
+
+            Assert.Equal(0, questCount);
+        }
+
+        [Fact]
+        public void QuestLog_A_Single_QuestItem_Added_Increases_Quest_Count_To_One()
+        {
+            var questLog = new QuestLog();
+            var questItem = new QuestItem("5 km forest hike", "Walk 5 km following a forest path.");
+            questLog.AddQuest(questItem);
+
+            var questCount = questLog.QuestItems.Count();
+
+            Assert.Equal(1, questCount);
+        }
+
+        [Fact]
+        public void QuestLog_A_Single_QuestItem_Added_Can_Be_Retrieved_By_ID()
+        {
+            var questLog = new QuestLog();
+            var questItem = new QuestItem("5 km forest hike", "Walk 5 km following a forest path.");
+            var questId = questItem.Id;
+
+            questLog.AddQuest(questItem);
+
+            var retrievedQuest = questLog.GetQuestById(questId);
+
+            Assert.Equal(questItem.Id, retrievedQuest.Id);
+        }
+    }
+}
