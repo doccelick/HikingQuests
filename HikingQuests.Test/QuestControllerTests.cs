1fd950b63e8d0cd19bf25f0f4c101bbb7b095225
Anders Evjen Bakke <aeb@ananki.no>
2025-09-11 23:16:56 +0200

    Added an initial test for QuestController using Moq

---
+++ HikingQuests.Test/QuestControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using HikingQuests.Server.Controllers;
+using HikingQuests.Server.Models;
+using Moq;
+
+namespace HikingQuests.Test
+{
+    public class QuestControllerTests
+    {
+        [Fact]
+        public void GetQuests_Returns_All_Quests()
+        {
+            var mockQuestLog = new Mock<IQuestLog>();
+                        
+            mockQuestLog.Setup(q => q.GetAllQuestItems()).Returns(new List<QuestItem>
+            {
+                new QuestItem("Quest 1", "Description 1"),
+                new QuestItem("Quest 2", "Description 2"),
+                new QuestItem("Quest 3", "Description 3")
+            });
+
+            var controller = new QuestController(mockQuestLog.Object);
+
+            var result = controller.GetQuests();
+
+            var quests = Assert.IsAssignableFrom<IEnumerable<QuestItem>>(result);
+            Assert.Equal(3, quests.Count());
+        }
+    }
+}
