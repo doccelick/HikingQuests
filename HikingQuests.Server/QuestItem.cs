8889cb86e25254b42358a21cb5a3b6d5cb902081
Anders Evjen Bakke <aeb@ananki.no>
2025-09-11 18:50:03 +0200

    Implemented basic functionality for adding and retrieving a single QuestItem in QuestLog

--- HikingQuests.Server/QuestItem.cs
+++ HikingQuests.Server/QuestItem.cs
@@ -2,6 +2,7 @@
 {
     public class QuestItem
     {
+        public Guid Id { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
         public QuestStatus Status { get; private set; }
@@ -17,7 +18,7 @@
             {
                 throw new ArgumentException(QuestMessages.DescriptionCannotBeNullOrEmpty);
             }
-
+            Id = Guid.NewGuid();
             Title = title;            
             Description = description;
             Status = QuestStatus.Planned;
---
+++ HikingQuests.Server/QuestLog.cs
@@ -0,0 +1,20 @@
+﻿namespace HikingQuests.Server
+{
+    public class QuestLog
+    {
+        public QuestLog() { }
+
+        private Dictionary<Guid, QuestItem> _questItems = new Dictionary<Guid, QuestItem>();
+        public IEnumerable<QuestItem> QuestItems => _questItems.Values;
+
+        public void AddQuest(QuestItem questItem)
+        {            
+            _questItems[questItem.Id] = questItem;
+        }
+
+        public QuestItem GetQuestById(Guid questId)
+        {            
+            return _questItems[questId];
+        }
+    }
+}
