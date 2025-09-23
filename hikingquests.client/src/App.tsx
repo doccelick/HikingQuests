9f2a3aa6e71a480d1d0f92da676f8bfa7bb71c31
Anders Evjen Bakke <aeb@ananki.no>
2025-09-23 10:02:42 +0200

    Updated App.tsx to point correctly to QuestList.tsx

--- hikingquests.client/src/App.tsx
+++ hikingquests.client/src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css';
+import QuestList from "./components/QuestList";
 
 function App() {
+    return (
+        <div className="App">
+            <h1>Hiking Quests</h1>
+            <QuestList />
+        </div>
+    );
 }
 
 export default App;
