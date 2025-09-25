9ac77edd70f0db71b83beb184fc9d8ca4920642e
Anders Evjen Bakke <aeb@ananki.no>
2025-09-25 17:18:23 +0200

    Added a QuestCard properties interface

---
+++ hikingquests.client/src/features/quests/types/QuestCardProperties.ts
@@ -0,0 +1,8 @@
+import type { QuestItem } from "./QuestItem";
+
+export interface QuestCardProperties {
+    quest: QuestItem;
+    expanded: boolean;
+    onExpandToggle: () => void;
+    onStartQuest: () => void;
+}
