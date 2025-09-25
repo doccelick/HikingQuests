27746e2fafd82d1021bddedcfe53969a8c3701c9
Anders Evjen Bakke <aeb@ananki.no>
2025-09-25 23:04:08 +0200

    Updated QuestCardProperties with OnUpdateQuest

--- hikingquests.client/src/features/quests/types/QuestCardProperties.ts
+++ hikingquests.client/src/features/quests/types/QuestCardProperties.ts
@@ -1,4 +1,5 @@
 import type { QuestItem } from "./QuestItem";
+import type { UpdateQuestDto } from "./UpdateQuestDto";
 
 export interface QuestCardProperties {
     quest: QuestItem;
@@ -6,4 +7,5 @@ export interface QuestCardProperties {
     onExpandToggle: () => void;
     onStartQuest: () => void;
     onCompleteQuest: () => void;
+    onUpdateQuest: (id: string, dto: UpdateQuestDto) => Promise<void>;
 }
