64b57a48a6551506c924af4af3fc1ad4be001c96
Anders Evjen Bakke <aeb@ananki.no>
2025-09-25 22:59:54 +0200

    Created a QuestResponseDto

---
+++ HikingQuests.Server/Dtos/QuestResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HikingQuests.Server.Dtos
+{
+    public class QuestResponseDto
+    {public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
