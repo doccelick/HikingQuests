2bb78a7d272ec760f4f23f1491a373ec739cb8f7
Anders Evjen Bakke <aeb@ananki.no>
2025-09-25 16:33:12 +0200

    Updated questlog interface

--- HikingQuests.Server/Models/IQuestLog.cs
+++ HikingQuests.Server/Models/IQuestLog.cs
@@ -2,13 +2,13 @@
 {
     public interface IQuestLog
     {
-        void AddQuest(QuestItem quest);
+        QuestItem AddQuest(QuestItem quest);
         QuestItem GetQuestById(Guid id);
         QuestItem GetQuestByTitle(string title);
         IEnumerable<QuestItem> GetAllQuestItems();
         void UpdateQuestTitle(Guid id, string newTitle);
         void UpdateQuestDescription(Guid id, string newDescription);
-        void StartQuest(Guid id);
+        QuestItem StartQuest(Guid id);
         void CompleteQuest(Guid id);
         void DeleteQuest(Guid id);
     }
