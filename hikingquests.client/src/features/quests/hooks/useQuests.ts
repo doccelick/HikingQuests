6217c60f7eeffa773a429cbf4688b90a57760646
Anders Evjen Bakke <aeb@ananki.no>
2025-09-25 16:41:36 +0200

    Added a startQuestHandler

--- hikingquests.client/src/features/quests/hooks/useQuests.ts
+++ hikingquests.client/src/features/quests/hooks/useQuests.ts
@@ -1,13 +1,20 @@
 import type { QuestItem } from "../types/QuestItem";
-import { getQuests } from "../services";
+import { getQuests, startQuest } from "../services";
 import { useRetryableFetch } from "./useRetryableFetch";
 
 export function useQuests() {
 
-    const { data, loading, error } = useRetryableFetch<QuestItem[]>(getQuests, {
+    const { data, loading, error, refetch } = useRetryableFetch<QuestItem[]>(getQuests, {
         retries: 5,
         delay: 500,
     });
 
-    return { quests: data ?? [], loading, error };
+    const startQuestHandler = (id: string) => startQuest(id).then(refetch);
+
+    return {
+        quests: data ?? [],
+        loading,
+        error,
+        startQuestHandler
+    };
 }
