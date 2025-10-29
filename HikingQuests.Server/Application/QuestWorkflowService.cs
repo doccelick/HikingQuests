c9983ac8570db0abbfd76a24ac9f08370ea47ad2
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:24:11 +0100

    Added QuestWorkflowService

---
+++ HikingQuests.Server/Application/QuestWorkflowService.cs
@@ -0,0 +1,26 @@
+﻿using HikingQuests.Server.Application.Interfaces;
+using HikingQuests.Server.Infrastructure;
+
+namespace HikingQuests.Server.Application
+{
+    public class QuestWorkflowService : IQuestWorkflowService
+    {
+        private readonly IQuestRepository _repository;
+
+        public QuestWorkflowService(IQuestRepository repository) => _repository = repository;
+
+        public async Task CompleteQuestAsync(Guid id)
+        {
+            var questItem = await _repository.GetByIdAsync(id);
+            questItem.CompleteQuest();
+            await _repository.UpdateAsync(questItem);
+        }
+
+        public async Task StartQuestAsync(Guid id)
+        {
+            var questItem = await _repository.GetByIdAsync(id);
+            questItem.StartQuest();
+            await _repository.UpdateAsync(questItem);
+        }
+    }
+}
