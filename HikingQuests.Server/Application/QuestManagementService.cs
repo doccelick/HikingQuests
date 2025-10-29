d22c775e1a6863acdf80b31434db9d91b1dc24a1
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:23:31 +0100

    Added QuestManagementService

---
+++ HikingQuests.Server/Application/QuestManagementService.cs
@@ -0,0 +1,43 @@
+﻿using HikingQuests.Server.Application.Dtos;
+using HikingQuests.Server.Application.Interfaces;
+using HikingQuests.Server.Constants;
+using HikingQuests.Server.Domain.Entities;
+using HikingQuests.Server.Infrastructure;
+
+namespace HikingQuests.Server.Application
+{
+    public class QuestManagementService : IQuestManagementService
+    {
+        private readonly IQuestRepository _repository;
+                
+        public QuestManagementService(IQuestRepository repository) => _repository = repository;
+
+        public async Task<QuestItem> AddQuestAsync(AddQuestDto addQuestDto)
+        {
+            var newQuestItem = new QuestItem(addQuestDto.Title, addQuestDto.Description);
+
+            await _repository.AddAsync(newQuestItem);
+
+            return newQuestItem;
+        }
+
+        public async Task UpdateQuestAsync(Guid id, UpdateQuestDto updateQuestDto)
+        {
+            var questItem = await _repository.GetByIdAsync(id);
+
+            if (questItem is null)
+            {
+                throw new KeyNotFoundException(QuestMessages.QuestNotFound);
+            }
+
+            questItem.UpdateDescription(updateQuestDto.Description);
+            questItem.UpdateTitle(updateQuestDto.Title);
+            await _repository.UpdateAsync(questItem);
+        }
+                        
+        public async Task DeleteQuestAsync(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
