14f997060207c4a10de741eb2921da3a396bbc0a
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:01:41 +0100

    Created a Quest Repository interface

---
+++ HikingQuests.Server/Infrastructure/IQuestRepository.cs
@@ -0,0 +1,14 @@
+﻿using HikingQuests.Server.Domain.Entities;
+
+namespace HikingQuests.Server.Infrastructure
+{
+    public interface IQuestRepository
+    {
+        Task AddAsync(QuestItem questItem);
+        Task<QuestItem> GetByIdAsync(Guid id);
+        Task<QuestItem> GetByTitleAsync(string title);
+        Task<IEnumerable<QuestItem>> GetAllQuestsAsync();
+        Task UpdateAsync(QuestItem questItem);
+        Task DeleteAsync(Guid id);
+    }
+}
