4b20931fa9b0d678260a6012dd9e8ff0b1f0e961
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:19:25 +0100

    Added UnitOfWork class

---
+++ HikingQuests.Server/Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using HikingQuests.Server.Application.Interfaces;
+
+namespace HikingQuests.Server.Infrastructure.Persistence
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly QuestDbContext _questDbContext;
+
+        public UnitOfWork(QuestDbContext questDbContext) => _questDbContext = questDbContext;
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) 
+            => await _questDbContext.SaveChangesAsync(cancellationToken);
+
+        public void Dispose() => _questDbContext.Dispose();
+    }
+}
