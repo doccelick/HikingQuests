5843e308478a57cfbdebf16c110839c8a16efe9b
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:24:46 +0100

    Added AddQuestDto

---
+++ HikingQuests.Server/Application/Dtos/AddQuestDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HikingQuests.Server.Application.Dtos
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddQuestDto
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
