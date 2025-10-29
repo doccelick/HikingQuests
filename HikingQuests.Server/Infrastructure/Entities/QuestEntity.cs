e53a920584dafbb460d1e53c1924057a0b9729de
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 18:56:20 +0100

    Added QuestDbContext and QuestEntity

---
+++ HikingQuests.Server/Infrastructure/Entities/QuestEntity.cs
@@ -0,0 +1,12 @@
+﻿using HikingQuests.Server.Domain.ValueObjects;
+
+namespace HikingQuests.Server.Infrastructure.Entities
+{
+    public class QuestEntity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public QuestStatus Status { get; set; }
+    }
+}
