2abcc3b524ee06cf5c4f03b08facec60f8b183a4
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:23:48 +0100

    Added QuestQueryService

---
+++ HikingQuests.Server/Application/QuestQueryService.cs
@@ -0,0 +1,29 @@
+﻿using HikingQuests.Server.Application.Interfaces;
+using HikingQuests.Server.Domain.Entities;
+using HikingQuests.Server.Infrastructure;
+
+namespace HikingQuests.Server.Application
+{
+    public class QuestQueryService : IQuestQueryService
+    {
+        private readonly IQuestRepository _repository;
+
+        public QuestQueryService(IQuestRepository repository) => _repository = repository;
+
+
+        public async Task<IEnumerable<QuestItem>> GetAllQuestsAsync()
+        {
+            return await _repository.GetAllQuestsAsync();
+        }
+
+        public async Task<QuestItem> GetQuestByIdAsync(Guid id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<QuestItem> GetQuestByTitleAsync(string title)
+        {
+            return await _repository.GetByTitleAsync(title);
+        }
+    }
+}
