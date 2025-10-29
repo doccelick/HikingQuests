da883773be8b59d6392f5dc4c176eaf98bd2573f
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:16:57 +0100

    Added QuestMapper for entity conversion

---
+++ HikingQuests.Server/Infrastructure/Mapping/QuestMapper.cs
@@ -0,0 +1,33 @@
+﻿using HikingQuests.Server.Domain.Entities;
+using HikingQuests.Server.Infrastructure.Entities;
+
+namespace HikingQuests.Server.Infrastructure.Mapping
+{
+    public class QuestMapper
+    {
+        public static QuestEntity ToEntity(QuestItem questItem)
+        {
+            var entity = new QuestEntity() 
+            {
+                Id = questItem.Id,
+                Title = questItem.Title,
+                Description = questItem.Description,
+                Status = questItem.Status
+            };
+
+            return entity;
+        }
+
+        public static QuestItem ToDomain(QuestEntity entity)
+        {
+            var item = new QuestItem(
+                entity.Id,
+                entity.Title,
+                entity.Description,
+                entity.Status
+                );
+
+            return item;
+        }
+    }
+}
