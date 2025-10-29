aed6495ec2a6131e7d2c2f49c07a64cfdb45f8a2
Anders Evjen Bakke <aeb@ananki.no>
2025-10-29 19:05:04 +0100

    Added a test fixture for SQLite to facilitate integration testing

---
+++ HikingQuests.Test/Infrastructure/PersistanceTests/SqliteQuestTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using HikingQuests.Server.Infrastructure.Persistence;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace HikingQuests.Tests.Infrastructure.PersistanceTests
+{
+    public class SqliteQuestTestFixture : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+
+        public DbContextOptions<QuestDbContext> ContextOptions { get; }
+
+        public SqliteQuestTestFixture()
+        {
+            _connection = new SqliteConnection("Datasource=:memory:");
+            _connection.Open();
+
+            ContextOptions = new DbContextOptionsBuilder<QuestDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            using (var context = new QuestDbContext(ContextOptions))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public QuestDbContext CreateContext() => new QuestDbContext(ContextOptions);
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
